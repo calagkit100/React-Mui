d27eda078f5048d13b85971bdacc076a53ab1908
calagkit100 <calagkit100@gmail.com>
2023-01-13 15:04:12 +0800

    MuiSelect: Select components, dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { MuiSelect } from './components/MuiSelect';
 import { MuiTextField } from './components/MuiTextField';
 // import { MuiButton } from './components/MuiButton';
 // import { MuiTypography } from './components/MuiTypography';
@@ -10,7 +11,8 @@ function App() {
     <div className="App">
       {/* <MuiTypography /> */}
       {/* <MuiButton /> */}
-      <MuiTextField />
+      {/* <MuiTextField /> */}
+      <MuiSelect />
     </div>
   );
 }
